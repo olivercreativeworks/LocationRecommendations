60ea3d03771bd14ae9a9b5956dc338b6b7189403
Oliver <olivercreativeworks@gmail.com>
2023-01-09 13:43:53 -0800

    Initial Commit

---
+++ RealtorProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealtorProject
+//
+//  Created by Oliver Allen-Cummings on 1/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
