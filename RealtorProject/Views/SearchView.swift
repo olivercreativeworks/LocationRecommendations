cff07dcc6937400d5548381cb4f9716348593d68
Oliver <olivercreativeworks@gmail.com>
2023-01-09 17:41:07 -0800

    Add search bar and buttons to Search View Controller.

--- RealtorProject/Views/SearchView.swift
+++ RealtorProject/Views/SearchView.swift
@@ -17,11 +17,12 @@ class SearchView: UIView {
     var buttonStackView = UIStackView()
     
     func configure(){
-        self.backgroundColor = .systemBackground
         self.greetingLabel = configureGreetingLabel()
+        self.buttonStackView = configureButtonStackView()
+        self.searchBar = configureSearchBar()
     }
     
-    func configureGreetingLabel() -> UILabel{
+    private func configureGreetingLabel() -> UILabel{
         greetingLabel.text = "Hey, World. How are you?"
         
         // Set font wrapping
@@ -49,4 +50,79 @@ class SearchView: UIView {
         ])
         return greetingLabel
     }
+    
+    private func configureSearchBar() -> UISearchBar{
+        searchBar.placeholder = "Where would you like to go?"
+
+        addSubview(searchBar)
+
+        // Set constraints
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+//            searchBar.topAnchor.constraint(equalTo: buttonStackView.bottomAnchor, constant: 40),
+            searchBar.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+            searchBar.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            searchBar.widthAnchor.constraint(equalTo: self.safeAreaLayoutGuide.widthAnchor, constant: -20)
+        ])
+        self.backgroundColor = .systemBackground
+        self.greetingLabel = configureGreetingLabel()
+        return searchBar
+    }
+    
+    private func configureButton(_ button:UIButton){
+        var config = UIButton.Configuration.filled()
+        config.titlePadding = 10
+        config.baseBackgroundColor = .secondarySystemBackground
+        button.configuration = config
+        
+//            $0.backgroundColor = .secondarySystemBackground
+        button.titleLabel?.lineBreakMode = .byWordWrapping
+        button.titleLabel?.numberOfLines = 0
+
+        button.setTitleColor(.label, for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 25
+    }
+    
+    private func placeButtonInStack(_ button:UIButton, _ buttonStackView: UIStackView){
+        buttonStackView.addArrangedSubview(button)
+
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            button.heightAnchor.constraint(equalTo: buttonStackView.heightAnchor),
+            button.widthAnchor.constraint(lessThanOrEqualTo: button.heightAnchor)
+        ])
+    }
+    
+    private func configureButtonStackView() -> UIStackView{
+        buttonStackView.axis = .horizontal
+//        buttonStackView.distribution = .fill
+        buttonStackView.distribution = .fillEqually
+        buttonStackView.alignment = .center
+        buttonStackView.spacing = 15
+        let buttons = [hungryButton, workoutButton, boredButton]
+        
+        hungryButton.setTitle("I'm hungry", for: .normal)
+        workoutButton.setTitle("I wanna workout", for: .normal)
+        boredButton.setTitle("I'm bored", for: .normal)
+        hungryButton.titleLabel?.lineBreakStrategy = .standard
+        
+        buttons.forEach{
+            configureButton($0)
+            placeButtonInStack($0, buttonStackView)
+        }
+        
+        addSubview(buttonStackView)
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            buttonStackView.topAnchor.constraint(equalTo: greetingLabel.bottomAnchor, constant: 40),
+            buttonStackView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            buttonStackView.widthAnchor.constraint(lessThanOrEqualTo: self.safeAreaLayoutGuide.widthAnchor)
+        ])
+        
+        return buttonStackView
+    }
+
 }
