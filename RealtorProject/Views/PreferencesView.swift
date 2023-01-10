69ef7c256b8fface2686d6ecbe23d76ac6238b51
Oliver <olivercreativeworks@gmail.com>
2023-01-09 20:07:34 -0800

    Add delegate to Search View and connected search view controller to preferences view controller.

---
+++ RealtorProject/Views/PreferencesView.swift
@@ -0,0 +1,18 @@
+//
+//  PreferencesView.swift
+//  RealtorProject
+//
+//  Created by Oliver Allen-Cummings on 1/9/23.
+//
+
+import UIKit
+
+class PreferencesView: UIView {
+   
+    
+    func configure(){
+        self.backgroundColor = .systemBackground
+    }
+    
+   
+}
--- RealtorProject/Views/SearchView.swift
+++ RealtorProject/Views/SearchView.swift
@@ -7,16 +7,45 @@
 
 import UIKit
 
+protocol SearchViewDelegate{
+    func tappedHungryButton()
+    func tappedWorkoutButton()
+    func tappedBoredButton()
+    func tappedSearchBarButton()
+}
+
+
 class SearchView: UIView {
     
     var greetingLabel = UILabel()
-    var hungryButton = UIButton()
-    var workoutButton = UIButton()
-    var boredButton = UIButton()
+    var hungryButton: UIButton?
+    var workoutButton: UIButton?
+    var boredButton: UIButton?
     var searchBar = UISearchBar()
     var buttonStackView = UIStackView()
     
+//    var greetingLabel = UILabel()
+//    var hungryButton = UIButton(primaryAction: UIAction(title: "I'm Hungry", handler: {_ in
+//        self.delegate?.tappedHungryButton()
+//    }))
+//    var workoutButton = UIButton()
+//    var boredButton = UIButton()
+//    var searchBar = UISearchBar()
+//    var buttonStackView = UIStackView()
+    
+    var delegate: SearchViewDelegate?
+    
     func configure(){
+        hungryButton = UIButton(primaryAction: UIAction(title: "I'm Hungry", handler: { [weak self] _ in
+            self?.delegate?.tappedHungryButton()
+        }))
+        workoutButton = UIButton(primaryAction: UIAction(title: "I wanna Workout", handler: { [weak self] _ in
+            self?.delegate?.tappedWorkoutButton()
+        }))
+        boredButton = UIButton(primaryAction: UIAction(title: "I'm Bored", handler: {[weak self] _ in
+            self?.delegate?.tappedBoredButton()
+        }))
+        self.backgroundColor = .systemBackground
         self.greetingLabel = configureGreetingLabel()
         self.buttonStackView = configureButtonStackView()
         self.searchBar = configureSearchBar()
@@ -45,7 +74,7 @@ class SearchView: UIView {
         greetingLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             greetingLabel.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor),
-            greetingLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+//            greetingLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
             greetingLabel.widthAnchor.constraint(equalTo: self.safeAreaLayoutGuide.widthAnchor, constant: -20)
         ])
         return greetingLabel
@@ -64,8 +93,7 @@ class SearchView: UIView {
             searchBar.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             searchBar.widthAnchor.constraint(equalTo: self.safeAreaLayoutGuide.widthAnchor, constant: -20)
         ])
-        self.backgroundColor = .systemBackground
-        self.greetingLabel = configureGreetingLabel()
+
         return searchBar
     }
     
@@ -103,23 +131,26 @@ class SearchView: UIView {
         buttonStackView.spacing = 15
         let buttons = [hungryButton, workoutButton, boredButton]
         
-        hungryButton.setTitle("I'm hungry", for: .normal)
-        workoutButton.setTitle("I wanna workout", for: .normal)
-        boredButton.setTitle("I'm bored", for: .normal)
-        hungryButton.titleLabel?.lineBreakStrategy = .standard
+//        hungryButton.setTitle("I'm hungry", for: .normal)
+        workoutButton?.setTitle("I wanna workout", for: .normal)
+        boredButton?.setTitle("I'm bored", for: .normal)
+        hungryButton?.titleLabel?.lineBreakStrategy = .standard
         
         buttons.forEach{
-            configureButton($0)
-            placeButtonInStack($0, buttonStackView)
+            guard let button = $0 else {return}
+            configureButton(button)
+            placeButtonInStack(button, buttonStackView)
         }
         
         addSubview(buttonStackView)
         buttonStackView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            buttonStackView.topAnchor.constraint(equalTo: greetingLabel.bottomAnchor, constant: 40),
+//            buttonStackView.topAnchor.constraint(equalTo: greetingLabel.bottomAnchor, constant: 40),
             buttonStackView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            buttonStackView.widthAnchor.constraint(lessThanOrEqualTo: self.safeAreaLayoutGuide.widthAnchor)
+            buttonStackView.widthAnchor.constraint(lessThanOrEqualTo: self.safeAreaLayoutGuide.widthAnchor),
+            buttonStackView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            greetingLabel.bottomAnchor.constraint(equalTo: buttonStackView.topAnchor, constant: -40)
         ])
         
         return buttonStackView
